4530bf43f807680da98b191c2ba620853f500bfc
Mariela Campos <102038579+marielacamposss@users.noreply.github.com>
2022-06-07 21:52:20 -0400

    correcciones primera entrega

--- proyecto.js
+++ proyecto.js
@@ -15,75 +15,64 @@ const plantas = [
     }
 ]
 
+const nombresPlantas = plantas.map((planta) => planta.nombre);
+const nombresPlantasTexto = nombresPlantas.join(',')
+
 alert('bienvenido a cuida tu planta')
-cuidaTuplanta = prompt('¿Quieres cuidar una planta? (si/no)').toLowerCase()
+let cuidaTuplanta = prompt('¿Quieres cuidar una planta? (si/no)').toLowerCase()
 if (cuidaTuplanta == 'si') {
     let informacionPlanta = prompt('te gustaria saber las caracteristicas de cada planta (Si/No)? (ingresa no para continuar al cuidado de tu planta)').toLowerCase()
     if (informacionPlanta == 'si') {
-        let quieroInformacion = prompt('¿de que planta quieres saber? (filodendro,suculenta)').toLowerCase()
+        let quieroInformacion = prompt('¿de que planta quieres saber?' + nombresPlantasTexto).toLowerCase()
         let encontrar = quieroInformacion
         let encontrado = plantas.find((x) => x.nombre === encontrar)
         alert('los cuidados de los ' + encontrado.nombre + ' son: riego de ' + encontrado.riego + ' y luz solar de ' + encontrado.luzSolar)
     }
     else {
-        let tipoPlanta = prompt('¿que planta quieres cuidar? ' + 'Elige una de la siguiente lista e ingresala: ' + plantas[0].nombre + ' ' + plantas[1].nombre).toLowerCase()
-        alert('elegiste un ' + tipoPlanta)
+
+        let tipoPlanta = prompt('¿que planta quieres cuidar?' + nombresPlantasTexto).toLowerCase();
+        let plantaEncontrada = plantas.find((x) => x.nombre === tipoPlanta);
+
+        while (!plantaEncontrada) {
+            tipoPlanta = prompt('¿que planta quieres cuidar?').toLowerCase();
+            plantaEncontrada = plantas.find((x) => x.nombre === tipoPlanta);
+        }
+        alert('elegiste un ' + tipoPlanta);
 
         let nombrePlanta = prompt('¿Como quieres que se llame tu ' + tipoPlanta)
-        if (nombrePlanta == '') {
+        while (nombrePlanta.trim() === '') {
             alert('El nombre de tu planta no puede quedar vacio')
+            nombrePlanta = prompt('¿Como quieres que se llame tu ' + tipoPlanta);
         }
-        else {
+        if (nombrePlanta.trim() != '') {
             alert('El nombre de tu ' + tipoPlanta + ' ahora es ' + nombrePlanta)
 
-            if (tipoPlanta == 'filodendro') {
-
-                while (loop) {
-                    let regar = parseInt(prompt('cuanto quieres regar a ' + nombrePlanta + '?' + 'maximo ' + plantas[0].riego))
-                    if (!isNaN(regar)) {
-                        riegoActual += regar
-
-                        if (riegoActual == 5) {
-                            alert('Felicidades ' + nombrePlanta + ' Esta totalmente regada')
-                            break
-                        }
-                        else if (regar > 0 && regar <= 5) {
-                            alert(`regaste ${regar} veces a ${nombrePlanta} ahora su riego total es de ${riegoActual}`)
-                            alert('¿Quieres volver a regar a ' + nombrePlanta + '?')
-                        } else if (regar > 5) {
-                            alert('no puedes regar mas de 5 veces a ' + nombrePlanta)
-                        }
+            while (loop) {
+                let regar = prompt('cuanto quieres regar a ' + nombrePlanta + '?' + 'maximo ' + plantaEncontrada.riego + '(Si quiere dejar de regar su planta escriba SALIR)').toLowerCase()
+                if (regar == 'salir') {
+                    break
+                }
+                let regarNumero = parseInt(regar)
+                if (!isNaN(regarNumero)) {
+                    if (riegoActual > 0 && riegoActual < plantaEncontrada.riego && riegoActual + regarNumero < plantaEncontrada.riego) {
+                        riegoActual += regarNumero
+                        alert(`regaste ${regarNumero} veces a ${nombrePlanta} ahora su riego total es de ${riegoActual}`)
+                        alert('¿Quieres volver a regar a ' + nombrePlanta + '?')
+                    } else if (riegoActual > plantaEncontrada.riego) {
+                        alert('no puedes regar mas de' + plantaEncontrada.riego + ' veces a ' + nombrePlanta)
                     }
-                    else {
-                        alert('Debes ingresar un numero')
+                    else if (riegoActual == plantaEncontrada.riego) {
+                        alert('Felicidades ' + nombrePlanta + ' Esta totalmente regada')
+                        break
                     }
                 }
-            }
-            else if (tipoPlanta == 'suculenta') {
-                while (loop) {
-                    let regar = parseInt(prompt('cuanto quieres regar a ' + nombrePlanta + '?' + 'maximo ' + plantas[1].riego))
-                    if (!isNaN(regar)) {
-                        riegoActual += regar
-
-                        if (riegoActual == 3) {
-                            alert('Felicidades ' + nombrePlanta + ' Esta totalmente regada')
-                            break
-                        }
-                        else if (regar > 0 && regar <= 3) {
-                            alert(`regaste ${regar} veces a ${nombrePlanta} ahora su riego total es de ${riegoActual}`)
-                            alert('¿Quieres volver a regar a ' + nombrePlanta + '?')
-                        } else if (regar > 3) {
-                            alert('no puedes regar mas de 3 veces a ' + nombrePlanta)
-                        }
-                    }
-                    else {
-                        alert('Debes ingresar un numero')
-                    }
+                else {
+                    alert('Debes ingresar un numero')
                 }
             }
+
         }
     }
-
 }
 
 else if (cuidaTuplanta == 'no') {
