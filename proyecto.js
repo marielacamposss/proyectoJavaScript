b4e01a8ee4d873e26e78311d8ed5d51fad8b311e
Mariela Campos <102038579+marielacamposss@users.noreply.github.com>
2022-06-09 21:29:24 -0400

    Incorporar eventos

--- proyecto.js
+++ proyecto.js
@@ -1,8 +1,9 @@
 let loop = true
 let riegoActual = 0
-let botonEmpezar = document.getElementById('botonEmpezar')
-let botonCaracteristicas = document.getElementById('botonCaracteristicas')
-
+let botonEmpezar = document.getElementById('botonEmpezar');
+let botonCaracteristicas = document.getElementById('botonCaracteristicas');
+const formularioNombre = document.getElementById('formularioNombre');
+const resultDiv = document.getElementById('result');
 const plantas = [
     {
         nombre: 'filodendro',
@@ -14,75 +15,92 @@ const plantas = [
         riego: 3,
         luzSolar: 9
     }
-]
-
+];
 const nombresPlantas = plantas.map((planta) => planta.nombre);
-const nombresPlantasTexto = nombresPlantas.join(',')
+const nombresPlantasTexto = nombresPlantas.join(',');
 
-//no se bien como hacer que esto se ponga abajo de titulo y no abajo de todo
-let opcionesPlantas = document.createElement('h2')
-opcionesPlantas.innerHTML = `<h2>Puedes elegir entre todas las plantas que tenemos disponibles: ${nombresPlantasTexto}</h2>`;
+//PONERLE EL NOMBRE A LA PLANTA
 
-document.body.appendChild(opcionesPlantas);
+formularioNombre.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const nombrePlanta = document.getElementById('nombre').value;
+
+    if (verifyForm(nombrePlanta)) {
+        const formObject = {
+            name: nombrePlanta,
+        };
+
+        localStorage.setItem('formData', JSON.stringify(formObject));
+        fillResult();
+    }
+});
+
+const verifyForm = (nombrePlanta) => {
+    if (nombrePlanta.trim() === '') {
+        return false;
+    }
+    return true;
+};
+
+const fillResult = () => {
+    const formData = JSON.parse(localStorage.getItem('formData'));
+    resultDiv.innerHTML = `
+          <p>El nombre de tu planta es ${formData.name}</p>
+        `;
+}
+
+if (localStorage.getItem('formData')) {
+    fillResult();
+}
 
 //COMIENZA EL JUEGO
 botonEmpezar.onclick = () => {
-
+    const nombrePlanta = JSON.parse(localStorage.getItem('formData'))
     alert('bienvenido a cuida tu planta')
-    let cuidaTuplanta = prompt('¿Quieres cuidar una planta? (si/no)').toLowerCase()
+    let cuidaTuplanta = prompt('¿Quieres empezar a cuidar a ' + nombrePlanta.name + ' (si/no)').toLowerCase()
     if (cuidaTuplanta == 'si') {
 
-        let tipoPlanta = prompt('¿que planta quieres cuidar?' + nombresPlantasTexto).toLowerCase();
+        let tipoPlanta = prompt('Elige el tipo de planta que sera ' + nombrePlanta.name + ' Las opciones son: ' + nombresPlantasTexto).toLowerCase();
         let plantaEncontrada = plantas.find((x) => x.nombre === tipoPlanta);
 
         while (!plantaEncontrada) {
-            tipoPlanta = prompt('¿que planta quieres cuidar?').toLowerCase();
+            tipoPlanta = prompt('Elige el tipo de planta que sera ' + nombrePlanta.name + ' Las opciones son: ' + nombresPlantasTexto).toLowerCase();
             plantaEncontrada = plantas.find((x) => x.nombre === tipoPlanta);
         }
         alert('elegiste un ' + tipoPlanta);
 
-        let nombrePlanta = prompt('¿Como quieres que se llame tu ' + tipoPlanta)
-        while (nombrePlanta.trim() === '') {
-            alert('El nombre de tu planta no puede quedar vacio')
-            nombrePlanta = prompt('¿Como quieres que se llame tu ' + tipoPlanta);
-        }
-        if (nombrePlanta.trim() != '') {
-            alert('El nombre de tu ' + tipoPlanta + ' ahora es ' + nombrePlanta)
-
-            while (loop) {
-                let regar = prompt('cuanto quieres regar a ' + nombrePlanta + '?' + 'maximo ' + plantaEncontrada.riego + '(Si quiere dejar de regar su planta escriba SALIR)').toLowerCase()
-                if (regar == 'salir') {
+        while (loop) {
+            let regar = prompt('cuanto quieres regar a ' + nombrePlanta.name + '?' + 'maximo ' + plantaEncontrada.riego + '(Si quiere dejar de regar su planta escriba SALIR)').toLowerCase()
+            if (regar == 'salir') {
+                break
+            }
+            let regarNumero = parseInt(regar)
+            if (!isNaN(regarNumero)) {
+                if (riegoActual >= 0 && riegoActual < plantaEncontrada.riego && riegoActual + regarNumero <= plantaEncontrada.riego) {
+                    riegoActual += regarNumero
+                    alert(`regaste ${regarNumero} veces a ${nombrePlanta.name} ahora su riego total es de ${riegoActual}`)
+                    alert('¿Quieres volver a regar a ' + nombrePlanta.name + '?')
+                } else if (riegoActual > plantaEncontrada.riego) {
+                    alert('no puedes regar mas de' + plantaEncontrada.riego + ' veces a ' + nombrePlanta.name)
+                }
+                else if (riegoActual == plantaEncontrada.riego) {
+                    alert('Felicidades ' + nombrePlanta.name + ' Esta totalmente regada')
                     break
                 }
-                let regarNumero = parseInt(regar)
-                if (!isNaN(regarNumero)) {
-                    if (riegoActual >= 0 && riegoActual < plantaEncontrada.riego && riegoActual + regarNumero <= plantaEncontrada.riego) {
-                        riegoActual += regarNumero
-                        alert(`regaste ${regarNumero} veces a ${nombrePlanta} ahora su riego total es de ${riegoActual}`)
-                        alert('¿Quieres volver a regar a ' + nombrePlanta + '?')
-                    } else if (riegoActual > plantaEncontrada.riego) {
-                        alert('no puedes regar mas de' + plantaEncontrada.riego + ' veces a ' + nombrePlanta)
-                    }
-                    else if (riegoActual == plantaEncontrada.riego) {
-                        alert('Felicidades ' + nombrePlanta + ' Esta totalmente regada')
-                        break
-                    }
-                }
-                else {
-                    alert('Debes ingresar un numero')
-                }
             }
-
+            else {
+                alert('Debes ingresar un numero')
+            }
         }
-    }
 
+    }
     else if (cuidaTuplanta == 'no') {
         alert('no hay problema, que estes muy bien')
     }
-
     else {
         alert('tu respuesta no es valida, debe ser si o no')
     }
+
 }
 
 //CARACTERISTICAS
