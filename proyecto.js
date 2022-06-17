9b6e9104f570a25ee45edd6b1c28f03091ae1d15
Mariela Campos <102038579+marielacamposss@users.noreply.github.com>
2022-06-16 21:50:46 -0400

    segunda entrega proyecto final

--- proyecto.js
+++ proyecto.js
@@ -1,12 +1,17 @@
 let loop = true
 let riegoActual = 0
-let botonEmpezar = document.getElementById('botonEmpezar');
+let luzSolarActual = 0
+
+let botonRegar = document.getElementById('botonRegar');
+let botonLuzSolar = document.getElementById('botonLuzSolar');
 let botonCaracteristicas = document.getElementById('botonCaracteristicas');
 const formularioNombre = document.getElementById('formularioNombre');
 const formularioTipoPlanta = document.getElementById('formularioTipoPlanta');
 const resultDiv = document.getElementById('result');
 const resultTipoPlanta = document.getElementById('resultTipoPlanta');
 const resultCaractPlanta = document.getElementById('resultCaractPlanta');
+const resultRiegoPlanta = document.getElementById('resultRiegoPlanta');
+const resultLuzSolar = document.getElementById('resultLuzSolar')
 
 const plantas = [
     {
@@ -33,7 +38,7 @@ const nombresPlantas = plantas.map((planta) => planta.nombre);
 const nombresPlantasTexto = nombresPlantas.join(',');
 
 //PONERLE EL NOMBRE A LA PLANTA
-
+alert('Bienvenido a cuida tu planta')
 formularioNombre.addEventListener('submit', (e) => {
     e.preventDefault();
     const nombrePlanta = document.getElementById('nombre').value;
@@ -43,7 +48,7 @@ formularioNombre.addEventListener('submit', (e) => {
             name: nombrePlanta,
         };
 
-        localStorage.setItem('formData', JSON.stringify(formObject));
+        sessionStorage.setItem('formData', JSON.stringify(formObject));
         fillResult();
     }
 });
@@ -56,13 +61,13 @@ const verifyForm = (nombrePlanta) => {
 };
 
 const fillResult = () => {
-    const formData = JSON.parse(localStorage.getItem('formData'));
+    const formData = JSON.parse(sessionStorage.getItem('formData'));
     resultDiv.innerHTML = `
           <p>El nombre de tu planta es ${formData.name}</p>
         `;
 }
 
-if (localStorage.getItem('formData')) {
+if (sessionStorage.getItem('formData')) {
     fillResult();
 }
 
@@ -89,79 +94,50 @@ formularioTipoPlanta.addEventListener('click', (e) => {
         name: tipoPlanta,
 
     };
-    localStorage.setItem('formData2', JSON.stringify(formObject2));
+    //CARACTERISTICAS
+    let plantaSeleccionada = plantas.find((x) => x.nombre === tipoPlanta);
+    resultCaractPlanta.innerHTML = `
+    <h3>CARACTERISTICAS</h3>
+    <p>el riego de ${plantaSeleccionada.nombre} es de ${plantaSeleccionada.riego} y la cantidad maxima de luz solar que puede recibir es de ${plantaSeleccionada.luzSolar}</p>
+               `
+
+    sessionStorage.setItem('formData2', JSON.stringify(formObject2));
     fillResult();
 });
 
-const tipoPlanta = JSON.parse(localStorage.getItem('formData2'))
+const tipoPlanta = JSON.parse(sessionStorage.getItem('formData2'))
 console.log(tipoPlanta)
 let plantaSeleccionada = plantas.find((x) => x.nombre === tipoPlanta.name);
 
+
 if (plantaSeleccionada != '') {
     resultCaractPlanta.innerHTML = `
-    <p>el riego de ${plantaSeleccionada.nombre} es de ${plantaSeleccionada.riego} </p>
+    <h3>CARACTERISTICAS</h3>
+    <p>el riego maximo de ${plantaSeleccionada.nombre} es de ${plantaSeleccionada.riego} y la cantidad maxima de luz solar que puede recibir es de ${plantaSeleccionada.luzSolar} </p>
                `
 }
 
-
-
-//COMIENZA EL JUEGO
-botonEmpezar.onclick = () => {
-    const nombrePlanta = JSON.parse(localStorage.getItem('formData'))
-    alert('bienvenido a cuida tu planta')
-    let cuidaTuplanta = prompt('¿Quieres empezar a cuidar a ' + nombrePlanta.name + ' (si/no)').toLowerCase()
-    if (cuidaTuplanta == 'si') {
-
-        let tipoPlanta = prompt('Elige el tipo de planta que sera ' + nombrePlanta.name + ' Las opciones son: ' + nombresPlantasTexto).toLowerCase();
-        let plantaEncontrada = plantas.find((x) => x.nombre === tipoPlanta);
-
-        while (!plantaEncontrada) {
-            tipoPlanta = prompt('Elige el tipo de planta que sera ' + nombrePlanta.name + ' Las opciones son: ' + nombresPlantasTexto).toLowerCase();
-            plantaEncontrada = plantas.find((x) => x.nombre === tipoPlanta);
-        }
-        alert('elegiste un ' + tipoPlanta);
-
-        while (loop) {
-            if (riegoActual == plantaEncontrada.riego) {
-                alert('Felicidades ' + nombrePlanta.name + ' Esta totalmente regada')
-                break
-            } else {
-                let regar = prompt('cuanto quieres regar a ' + nombrePlanta.name + '?' + 'maximo ' + plantaEncontrada.riego + '(Si quiere dejar de regar su planta escriba SALIR)').toLowerCase()
-                if (regar == 'salir') {
-                    break
-                }
-                let regarNumero = parseInt(regar)
-                if (!isNaN(regarNumero)) {
-                    if (riegoActual >= 0 && riegoActual < plantaEncontrada.riego && riegoActual + regarNumero <= plantaEncontrada.riego) {
-                        riegoActual += regarNumero
-                        alert(`regaste ${regarNumero} veces a ${nombrePlanta.name} ahora su riego total es de ${riegoActual}`)
-                    } else if (riegoActual > plantaEncontrada.riego) {
-                        alert('no puedes regar mas de' + plantaEncontrada.riego + ' veces a ' + nombrePlanta.name)
-                    }
-                }
-                else {
-                    alert('Debes ingresar un numero')
-                }
-            }
-        }
-    }
-    else if (cuidaTuplanta == 'no') {
-        alert('no hay problema, que estes muy bien')
-    }
-    else {
-        alert('tu respuesta no es valida, debe ser si o no')
-    }
-
-}
-
-//CARACTERISTICAS
-
-botonCaracteristicas.onclick = () => {
-    let informacionPlanta = prompt('te gustaria saber las caracteristicas de cada planta (Si/No)? (ingresa no para continuar al cuidado de tu planta)').toLowerCase()
-    if (informacionPlanta == 'si') {
-        let quieroInformacion = prompt('¿de que planta quieres saber?' + nombresPlantasTexto).toLowerCase()
-        let encontrar = quieroInformacion
-        let encontrado = plantas.find((x) => x.nombre === encontrar)
-        alert('los cuidados de los ' + encontrado.nombre + ' son: riego de ' + encontrado.riego + ' y luz solar de ' + encontrado.luzSolar)
+//RIEGO
+botonRegar.onclick = () => {
+    if (riegoActual == plantaSeleccionada.riego) {
+        alert('Felicidades ' + plantaSeleccionada.nombre + ' Esta totalmente regada')
+    } else {
+        riegoActual += 1
+        resultRiegoPlanta.innerHTML = `
+    <p> ${riegoActual} /${plantaSeleccionada.riego} </p>
+               `
     }
 }
+
+//LUZ SOLAR
+botonLuzSolar.onclick = () => {
+    if (luzSolarActual == plantaSeleccionada.luzSolar) {
+        alert('Felicidades ' + plantaSeleccionada.nombre + ' Esta totalmente asoleada')
+    } else {
+        luzSolarActual += 1
+        resultLuzSolar.innerHTML = `
+    <p> ${luzSolarActual} /${plantaSeleccionada.luzSolar} </p>
+               `
+    }
+}
+
